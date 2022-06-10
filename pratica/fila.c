cb9e93bfb092aa2876d488bc14b990d706809aae
Lucas Mattos Vieira <lucasmattosvieira@pop-os.localdomain>
2022-06-10 13:43:34 -0300

    Criadas as funções de fila

--- pratica/fila.c
+++ pratica/fila.c
@@ -1,5 +1,45 @@
 # include "fila.h"
 
 Fila criaFila() {
-    
+    Fila fila;
+    fila.inicio = 0;
+    fila.fim = 0;
+    fila.nroElementos = 0;
+    return fila;
+}
+
+void insereFila(int n, Fila fila) {
+    if (filaCheia(fila) == 0) {
+        fila.elementos[fila.fim] = n;
+        fila.fim++;
+        if (fila.fim == 10) {
+            fila.fim = 0;
+        }
+        fila.nroElementos++;
+    } else {
+        printf("Fila cheia!\n\n");
+    }
+}
+
+int removeFila(Fila fila) {
+    int n = -1;
+    if (filaVazia(fila) == 0) {
+        n = fila.elementos[fila.inicio];
+        fila.inicio++;
+        if (fila.inicio == 10) {
+            fila.inicio = 0;
+        }
+        fila.nroElementos--;
+    } else {
+        printf("Fila vazia!\n\n");
+    }
+    return n;
+}
+
+int filaVazia(Fila fila) {
+    return (fila.nroElementos == 0) ? 1 : 0; 
+}
+
+int filaCheia(Fila fila) {
+    return (fila.nroElementos == 10) ? 1 : 0; 
 }
--- pratica/fila.h
+++ pratica/fila.h
@@ -1,9 +1,15 @@
 struct fila
 {
-    int fila[10];
+    int elementos[10];
     int inicio;
     int fim;
     int nroElementos;
 };
 
 typedef struct fila Fila; 
+
+Fila criaFila();
+void insereFila(int n, Fila fila);
+int removeFila(Fila fila);
+int filaVazia(Fila fila);
+int filaCheia(Fila fila);
